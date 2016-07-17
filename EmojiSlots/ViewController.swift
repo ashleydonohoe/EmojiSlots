a48ea90a63af3c6197477c996145b947a55bd9a4
Ashley Donohoe <ashleyrdonohoe@gmail.com>
2016-07-17 15:43:20 -0400

    finished adding functionality. layout needs some fixes due to warnings I can't seem to resolve

--- EmojiSlots/ViewController.swift
+++ EmojiSlots/ViewController.swift
@@ -10,6 +10,10 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var emoji1: UILabel!
+    @IBOutlet weak var emoji2: UILabel!
+    @IBOutlet weak var emoji3: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +24,12 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func spin(sender: AnyObject) {
+        emoji1.text = EmojiModel().getRandomEmoji()
+        emoji2.text = EmojiModel().getRandomEmoji()
+        emoji3.text = EmojiModel().getRandomEmoji()
+        
+    }
 
 }
 
